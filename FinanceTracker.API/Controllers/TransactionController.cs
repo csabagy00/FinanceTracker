75c2cf71da480a7e4ac2e3e6ecf4ae401a11cd42
Csaba Gyori <csaba.gyo00@gmail.com>
2026-06-02 17:25:20 +0200

    Implement the rest of the CRUD operations for transactions

--- FinanceTracker.API/Controllers/TransactionController.cs
+++ FinanceTracker.API/Controllers/TransactionController.cs
@@ -1,4 +1,5 @@
-﻿using FinanceTracker.Domain.Entities;
+﻿using FinanceTracker.Application.DTOs;
+using FinanceTracker.Domain.Entities;
 using FinanceTracker.Infrastructure.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -24,22 +25,98 @@ public class TransactionController : ControllerBase
     {
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-        var transactions = await _context.Transactions.Where(t => t.UserId == userId).ToListAsync();
+        var transactions = await _context.Transactions.Where(t => t.UserId == userId)
+            .Select(t => new TransactionDTO
+            {
+                Id = t.Id,
+                Title = t.Title,
+                Amount = t.Amount,
+                Date = t.Date,
+                Type = t.Type,
+                Category = t.Category.Name,
+
+            }).ToListAsync();
 
         return Ok(transactions);
     }
 
-    [HttpPost("AddTransaction")]
-    public async Task<IActionResult> CreateTransaction(Transaction transaction)
+    [HttpGet("TransactionById")]
+    public async Task<IActionResult> GetTransactionById(Guid id)
     {
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-        transaction.UserId = userId!;
+        var transaction = await _context.Transactions.Include(t => t.Category).FirstOrDefaultAsync(t => t.UserId == userId && t.Id == id);
+
+        if (transaction == null)
+            return NotFound("Transaction not found");
+
+        var dto = new TransactionDTO
+        {
+            Id = transaction.Id,
+            Title = transaction.Title,
+            Amount = transaction.Amount,
+            Date = transaction.Date,
+            Type = transaction.Type,
+            Category = transaction.Category.Name
+        };
+
+        return Ok(dto);
+    }
+
+    [HttpPost("AddTransaction")]
+    public async Task<IActionResult> CreateTransaction(CreateTransactionDTO transactionDto)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        var category = _context.Categories.FirstOrDefault(c => c.Name == transactionDto.Category);
+
+        if (category == null)
+            return BadRequest("Category does not exist");
+
+        var transaction = new Transaction
+        {
+            Id = new Guid(),
+            Title = transactionDto.Title,
+            Amount = transactionDto.Amount,
+            Date = transactionDto.Date,
+            Type = transactionDto.Type,
+            Category = category!,
+            UserId = userId!
+        };
 
         _context.Transactions.Add(transaction);
 
         await _context.SaveChangesAsync();
 
-        return Ok(transaction);
+        return Ok(transactionDto);
+    }
+
+    [HttpDelete("DeleteTransaction")]
+    public async Task<IActionResult> DeleteTransaction(Guid id)
+    {
+        var transaction = _context.Transactions.FirstOrDefault(t => t.Id == id);
+
+        if (transaction == null)
+            return NotFound("Transaction not found");
+
+        _context.Transactions.Remove(transaction);
+        await _context.SaveChangesAsync();
+
+        return Ok($"Transaction {transaction.Title} deleted successfully");
+    }
+
+    [HttpPatch("UdpateTransaction")]
+    public async Task<IActionResult> UpdateTransaction(TransactionDTO transactionDto)
+    {
+        var transaction = _context.Transactions.FirstOrDefault(t => t.Id == transactionDto.Id);
+
+        if (transaction == null)
+            return NotFound("Transaction not found");
+
+        _context.Entry(transaction).CurrentValues.SetValues(transactionDto);
+
+        await _context.SaveChangesAsync();
+
+        return Ok("Updated transaction");
     }
 }
