8da5a3c8f69a8528781e77d5c844bf4f7df23f46
Csaba Gyori <csaba.gyo00@gmail.com>
2026-07-07 18:43:23 +0200

    Update Category entity with UserId & add migrations

--- FinanceTracker.Domain/Entities/Category.cs
+++ FinanceTracker.Domain/Entities/Category.cs
@@ -1,10 +1,10 @@
-﻿using System.Transactions;
-
+﻿
 namespace FinanceTracker.Domain.Entities;
 
 public class Category
 {
     public Guid Id { get; set; }
+    public string UserId { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public ICollection<Transaction> Transactions { get; set; } = [];
 }
