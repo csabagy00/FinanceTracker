adacbe4a3855744cca0738b925b53e609ac6946b
Csaba Gyori <csaba.gyo00@gmail.com>
2026-07-07 18:43:41 +0200

    Add category DTOs

---
+++ FinanceTracker.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿using FinanceTracker.Domain.Entities;
+
+namespace FinanceTracker.Application.DTOs;
+
+public class CategoryDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ICollection<Transaction> Transactions { get; set; } = [];
+}
---
+++ FinanceTracker.Application/DTOs/CategoryNameDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace FinanceTracker.Application.DTOs;
+
+public class CategoryNameDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
